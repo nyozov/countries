be5ff23a05ee82900de2fac23ca6fd217abc5681
nikolay <n_yozov@hotmail.com>
2022-07-23 17:36:21 -0400

    screenshot for readme

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 import SelectedCountry from "./components/SelectedCountry";
-import { Routes, Route, useLocation } from "react-router-dom";
-import axios from 'axios';
-import {Country, Flag, Name} from './interfaces'
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import { Country } from "./interfaces";
 
 const App: React.FC = () => {
-  const [lightMode, setLightMode] = useState("light")
+  const [lightMode, setLightMode] = useState("light");
   const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
@@ -24,15 +24,18 @@ const App: React.FC = () => {
   }, []);
   return (
     <div className={`${lightMode} bg-gray-100 min-h-screen`}>
-      <div className='min-h-screen dark:bg-gray-900'>
-      <Navbar lightMode={lightMode} setLightMode={setLightMode} />
-      <Routes>
-        <Route path="/" element={<Homepage countries={countries}/>} />
-        <Route path="country/:name" element={<SelectedCountry countries={countries} />} />
-      </Routes>
+      <div className="min-h-screen dark:bg-gray-900">
+        <Navbar lightMode={lightMode} setLightMode={setLightMode} />
+        <Routes>
+          <Route path="/" element={<Homepage countries={countries} />} />
+          <Route
+            path="country/:name"
+            element={<SelectedCountry countries={countries} />}
+          />
+        </Routes>
       </div>
     </div>
   );
-}
+};
 
 export default App;
